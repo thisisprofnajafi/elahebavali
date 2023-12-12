81bbc0bcdee675d1fe1dd2d6303fa963a74acb35
Avin Dev <avindevelop@gmail.com>
2023-12-12 23:14:48 +0330

    ui has changed

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,118 +1,86 @@
-<!DOCTYPE html>
+<!doctype html>
+<html lang="en">
 
-<html
-    lang="en"
-    class="light-style customizer-hide"
-    dir="ltr"
-    data-theme="theme-default"
-    data-assets-path="../../assets/"
-    data-template="horizontal-menu-template"
->
 <head>
+
     <meta charset="utf-8" />
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
-    />
+    <title>Log in | Chatvia - Responsive Bootstrap 5 Chat App</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Responsive Bootstrap 5 Chat App" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
 
+    <!-- Bootstrap Css -->
+    <link href="{{asset('assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{asset('assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
 
-    <meta name="description" content="" />
-
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{asset('assets/img/favicon/favicon.ico')}}" />
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-        rel="stylesheet"
-    />
-
-    <!-- Icons -->
-    <link rel="stylesheet" href="{{asset('dist/css/style.min.css')}}" />
 
 </head>
 
 <body>
-<!-- Content -->
 
-<div class="container-xxl">
-    <div class="authentication-wrapper authentication-basic container-p-y">
-        <div class="authentication-inner py-4">
-            <!-- Login -->
-            <div class="card">
-                <div class="card-body">
-                    <!-- Logo -->
-                    <div class="app-brand justify-content-center mb-4 mt-2">
-                        <a href="" class="app-brand-link gap-2">
-                            <span class="app-brand-text demo text-body fw-bold ms-1">مدیر کانال</span>
-                        </a>
-                    </div>
-                    <!-- /Logo -->
 
-                    <p class="mb-4"></p>
-                    <form id="formAuthentication" class="mb-3" action="{{ route('login') }}" method="POST">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="email" class="form-label">ایمیل</label>
-                            <input
-                                type="email"
-                                class="form-control"
-                                id="email"
-                                name="email"
-                                placeholder="ایمیل خود را وارد کنید"
-                                autofocus
-                            />
-                        </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label">رمز عبور</label>
-                            <input
-                                type="password"
-                                class="form-control"
-                                id="password"
-                                name="password"
-                                placeholder="رمز عبور خود را وارد کنید"
-                            />
-                        </div>
-                        <div class="mb-3">
-                            <button class="btn btn-primary d-grid w-100" type="submit">ورود</button>
-                        </div>
-                    </form>
+<div class="account-pages my-5 pt-sm-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-lg-6 col-xl-5">
+                <div class="text-center mb-4">
+                    <h4>ورود</h4>
+                    <p class="text-muted mb-4">به مدیر کانال بله وارد شوید </p>
 
                 </div>
+
+                <div class="card">
+                    <div class="card-body p-4">
+                        <div class="p-3">
+                            <form action="{{ route('login') }}" method="POST">
+                                @csrf
+                                <div class="mb-3">
+                                    <label class="form-label">ایمیل</label>
+                                    <div class="input-group mb-3 bg-light-subtle rounded-3">
+                                                <span class="input-group-text text-muted" id="basic-addon3">
+                                                    <i class="ri-user-2-line"></i>
+                                                </span>
+                                        <input type="text" class="form-control form-control-lg border-light bg-light-subtle" placeholder="ایمیل خود را وارد کنید" aria-label="ایمیل خود را وارد کنید" aria-describedby="basic-addon3" name="email">
+                                    </div>
+                                </div>
+
+                                <div class="mb-4">
+                                    <label class="form-label">رمز عبور</label>
+                                    <div class="input-group mb-3 bg-light-subtle rounded-3">
+                                                <span class="input-group-text text-muted" id="basic-addon4">
+                                                    <i class="ri-lock-2-line"></i>
+                                                </span>
+                                        <input type="password" class="form-control form-control-lg border-light bg-light-subtle" placeholder="رمز عبور خود را وارد کنید" aria-label="رمز عبور خود را وارد کنید" aria-describedby="basic-addon4" name="password">
+                                    </div>
+                                </div>
+
+                                <div class="d-grid">
+                                    <button class="btn btn-primary waves-effect waves-light" type="submit">ورود</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-            <!-- /Register -->
         </div>
     </div>
 </div>
+<!-- end account-pages -->
 
-<!-- / Content -->
 
-<!-- Core JS -->
-<!-- build:js assets/vendor/js/core.js -->
-<script src="{{asset('assets/vendor/libs/jquery/jquery.js')}}"></script>
-<script src="{{asset('assets/vendor/libs/popper/popper.js')}}"></script>
-<script src="{{asset('assets/vendor/js/bootstrap.js')}}"></script>
-<script src="{{asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
-<script src="{{asset('assets/vendor/libs/node-waves/node-waves.js')}}"></script>
+<!-- JAVASCRIPT -->
+<script src="{{asset('assets/libs/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
+<script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
 
-<script src="{{asset('assets/vendor/libs/hammer/hammer.js')}}"></script>
-<script src="{{asset('assets/vendor/libs/i18n/i18n.js')}}"></script>
-<script src="{{asset('assets/vendor/libs/typeahead-js/typeahead.js')}}"></script>
+<script src="{{asset('assets/js/app.js')}}"></script>
 
-<script src="{{asset('assets/vendor/js/menu.js')}}"></script>
-<!-- endbuild -->
-
-<!-- Vendors JS -->
-<script src="{{asset('assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js')}}"></script>
-<script src="{{asset('assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js')}}"></script>
-<script src="{{asset('assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js')}}"></script>
-
-<!-- Main JS -->
-<script src="{{asset('assets/js/main.js')}}"></script>
-
-<!-- Page JS -->
-<script src="{{asset('assets/js/pages-auth.js')}}"></script>
 </body>
 </html>
